0cc51116e38c78aa9ab6dd513acee36b6f6eb8f4
Maxim <maksimkalin@mail.ru>
2023-06-28 17:19:06 +0300

    feat(ElementWrapper): добавил интерактивные элементы

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ActivityPage from "./pages/activity.page";
 import Map from "./pages/map.page";
