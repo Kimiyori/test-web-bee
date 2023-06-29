7e51addc68f0fe8f0a82ad8bec799499148a44e8
Maxim <maksimkalin@mail.ru>
2023-06-29 14:56:33 +0300

    fix: улучшил названия функций, немного улучшил стили

--- js/components/header.js
+++ js/components/header.js
@@ -8,75 +8,93 @@ class Header extends HTMLElement {
   }
 
   render() {
-    this.innerHTML = ` <header>
-      <section class="z-1 p-1 bg-light shadow-sm position-relative">
-        <figure class="fs-5  position-absolute top-50 start-0 translate-middle-y ms-5">
-          <img src="../assets/icons/Back.svg" alt="..." />
-          <span class="ms-2 fw-light">User pages - Profile</span>
-        </figure>
-          <ul class="nav fs-6 h-100 justify-content-end list-unstyled gap-3 p-0 me-5">
-            <li class="nav-item text-center ">
-              <a   href="#" class="align-middle text-decoration-none text-black">
-                <img  src="../assets/icons/Statistics.svg" alt="Stat icon" />
-                <p class="mb-0 mt-1  fw-light">Statistics</p>
-              </a>
-            </li>
-            <li class="nav-item text-center ms-3">
-              <a  href="#" class="align-middle text-decoration-none text-black">
-                <img  src="../assets/icons/Invoices.svg"  alt="Invoices icon" />
-                <p class="mb-0 mt-1 fw-light">Invoices</p>
-              </a>
-            </li>
-            <li class="nav-item text-center ms-3">
-              <a  href="#" class="align-middle text-decoration-none text-black">
-                <img  src="../assets/icons/Schedule.svg"  alt=".Schedule icon" />
-                <p class="mb-0 mt-1 fw-light">Schedule</p>
-              </a>
-            </li>
-          </ul>
-      </section>
-      <section class="row justify-content-evenly bg-white shadow-sm fs-6 ">
-        <ul class="col justify-content-start ms-5 nav  gap-3 " id="navigation">
-          <li class="nav-item  p-1 active-buttons d-inline-flex align-items-center">
-          <a href="/pages/activity.html" onclick="onClick(event)" class=" text-decoration-none text-black ms-2 fw-light ">
-              <img src="../assets/icons/Activity.svg"  alt="Activity icon" />
-             Activity
-              </a>
-          </li>
-          <li class="nav-item  p-1 active-buttons d-inline-flex align-items-center">
-          <a href="/pages/map.html" onclick="onClick(event)" class="align-baseline text-decoration-none text-black ms-2 fw-light">
-            <img src="../assets/icons/Map.svg"  alt="Map icon" />
-            Map</a>
-          </li>
-          <li class="nav-item  p-1 active-buttons d-inline-flex align-items-center">
-          <a href="/pages/time.html" onclick="onClick(event)" class="align-middle text-decoration-none text-black ms-2 fw-light">
-            <img src="../assets/icons/Time.svg"  alt="Time icon" />
-            Time</a>
-          </li>
-        </ul>
-        <ul class="col justify-content-end ms-3 nav  gap-3  me-5">
-          <li class="nav-item p-1 d-inline-flex align-items-center">
-          <img src="../assets/icons/Notes.svg"  alt="Notes icon" />
-            <a href="/" class="align-middle text-decoration-none text-black ms-2 fw-light">Notes</a>
-          </li>
-          <li class="nav-item   p-1 d-inline-flex align-items-center">
-          <img src="../assets/icons/Friends.svg"  alt="Friends icon" />
-            <a href="/" class="align-middle text-decoration-none text-black ms-2 fw-light">Friends</a>
-          </li>
-          <li class="nav-item  p-1 d-inline-flex align-items-center">
-          <img src="../assets/icons/Photo.svg"  alt="Photo icon" />
-            <a href="/" class="align-middle text-decoration-none text-black ms-2 fw-light">Photos</a>
-          </li>
-          <li class="nav-item  p-1 d-inline-flex align-items-center">
-          <img src="../assets/icons/Settings.svg"   alt="Settings icon" />
-            <a href="/" class="align-middle text-decoration-none text-black ms-2 fw-light"> Settings</a>
-          </li>
-          <li class="nav-item  p-1 d-inline-flex align-items-center">
-            <img src="../assets/icons/Dropdown.svg"  class="activity-icon" alt='Dropdown button' />
-          </li>
-        </ul>
-      </section>
-    </header>`;
+    this.innerHTML = `   
+  <header>
+    <section class="z-1 p-1 bg-light shadow-sm position-relative">
+      <figure class="fs-5 position-absolute top-50 start-0 translate-middle-y ms-5">
+        <img src="../assets/icons/Back.svg" alt="..." />
+        <span class="ms-2 fw-light">User pages - Profile</span>
+      </figure>
+      <ul class="nav fs-6 h-100 justify-content-end list-unstyled gap-3 p-0 me-5">
+        <li class="nav-item text-center">
+          <a href="#" class="align-middle text-decoration-none text-black">
+            <img src="../assets/icons/Statistics.svg" alt="Stat icon" />
+            <p class="mb-0 mt-1 fw-light">Statistics</p>
+          </a>
+        </li>
+        <li class="nav-item text-center ms-3">
+          <a href="#" class="align-middle text-decoration-none text-black">
+            <img src="../assets/icons/Invoices.svg" alt="Invoices icon" />
+            <p class="mb-0 mt-1 fw-light">Invoices</p>
+          </a>
+        </li>
+        <li class="nav-item text-center ms-3">
+          <a href="#" class="align-middle text-decoration-none text-black">
+            <img src="../assets/icons/Schedule.svg" alt=".Schedule icon" />
+            <p class="mb-0 mt-1 fw-light">Schedule</p>
+          </a>
+        </li>
+      </ul>
+    </section>
+    <section class="row justify-content-evenly bg-white shadow-sm fs-6">
+      <ul class="col justify-content-start ms-5 nav gap-1" id="navigation">
+        <li class="nav-item p-1 active-buttons">
+          <a
+            href="/pages/activity.html"
+            onclick="route(event)"
+            class="container text-decoration-none text-black fw-light"
+          >
+            <img src="../assets/icons/Activity.svg" alt="Activity icon" />
+            <span class="align-middle">Activity</span>
+          </a>
+        </li>
+        <li class="nav-item p-1 active-buttons">
+          <a href="/pages/map.html" onclick="route(event)" class="container text-decoration-none text-black fw-light">
+            <img src="../assets/icons/Map.svg" alt="Map icon" />
+            <span class="align-middle">Map</span>
+          </a>
+        </li>
+        <li class="nav-item p-1 active-buttons">
+          <a
+            href="/pages/time.html"
+            onclick="route(event)"
+            class="container text-decoration-none text-black fw-light"
+          >
+            <img src="../assets/icons/Time.svg" alt="Time icon" />
+            <span class="align-middle">Time</span>
+          </a>
+        </li>
+      </ul>
+      <ul class="col justify-content-end ms-3 nav gap-1 me-5">
+        <li class="nav-item p-1 d-inline-flex align-items-center">
+          <a href="/" class="container text-decoration-none text-black fw-light">
+            <img src="../assets/icons/Notes.svg" alt="Notes icon" />
+            <span class="align-middle">Notes</span>
+          </a>
+        </li>
+        <li class="nav-item p-1 d-inline-flex align-items-center">
+          <a href="/" class="container text-decoration-none text-black fw-light">
+            <img src="../assets/icons/Friends.svg" alt="Friends icon" />
+            <span class="align-middle">Friends</span>
+          </a>
+        </li>
+        <li class="nav-item p-1 d-inline-flex align-items-center">
+          <a href="/" class="container text-decoration-none text-black fw-light">
+            <img src="../assets/icons/Photo.svg" alt="Photo icon" />
+            <span class="align-middle">Photos</span>
+          </a>
+        </li>
+        <li class="nav-item p-1 d-inline-flex align-items-center">
+          <a href="/" class="container text-decoration-none text-black fw-light">
+            <img src="../assets/icons/Settings.svg" alt="Settings icon" />
+            <span class="align-middle">Settings</span>
+            <img src="../assets/icons/Dropdown.svg" width="20px" alt="Dropdown button" />
+          </a>
+        </li>
+      </ul>
+    </section>
+  </header>
+  `;
   }
 }
 
--- js/route.js
+++ js/route.js
@@ -1,21 +1,17 @@
-const onClick = (event) => {
-  if (window.location.origin.includes("kimiyori.github.io")) {
-    if (event.target.tagName.toLowerCase() === "a") {
-      event.target.pathname = "/test-web-bee" + event.target.pathname;
-    } else {
-      const anchor = event.target.closest("a");
-      anchor.pathname = "/test-web-bee" + anchor.pathname;
-    }
-  }
+const ROUTES = ["activity", "map", "time"];
+
+const route = (event) => {
+  const isNotLocalhost = window.location.origin.includes("kimiyori.github.io");
+  const target = event.target.tagName.toLowerCase() === "a" ? event.target : event.target.closest("a");
+  target.pathname = isNotLocalhost ? "/test-web-bee" + target.pathname : target.pathname;
 };
-const routes = ["activity", "map", "time"];
-function ready() {
+
+function highlightActiveButton() {
   const navElement = document.getElementById("navigation");
   const childs = navElement.querySelectorAll("li");
   for (let i = 0; i < childs.length; i++) {
-    if (window.location.pathname.includes(routes[i])) {
-      childs[i].classList.add("bg-body-secondary", "bg-opacity-50");
-    }
+    window.location.pathname.includes(ROUTES[i]) && childs[i].classList.add("bg-body-secondary", "bg-opacity-50");
   }
 }
-document.addEventListener("DOMContentLoaded", ready);
+
+document.addEventListener("DOMContentLoaded", highlightActiveButton);
