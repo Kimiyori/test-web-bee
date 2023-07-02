80a703bf0586686499990ac86671f9c463d53b3a
Maxim <maksimkalin@mail.ru>
2023-07-02 16:35:36 +0300

    feat: add skills-list component

---
+++ components/skillsList.js
@@ -0,0 +1,34 @@
+function escapeSpecialChars(jsonString) {
+  return jsonString.replace(/\n/g, "\\n").replace(/\r/g, "\\r").replace(/\t/g, "\\t").replace(/\f/g, "\\f");
+}
+class SkillsList extends HTMLElement {
+  connectedCallback() {
+    this.render();
+  }
+
+  static get observedAttributes() {
+    return ["image", "title", "alt"];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    this.render();
+  }
+
+  render() {
+    const title = this.attributes?.title.value;
+    const skills = JSON.parse(this.attributes?.skills.value);
+    this.innerHTML = `  
+        <article>
+            <h5 class="mb-4">${title}</h5>
+            <div class="d-flex flex-row gap-4">
+             ${skills.reduce(
+               (acc, skill) => acc + `<span class="bg-body-secondary bg-opacity-75 px-4 py-2 rounded">${skill}</span>`,
+               ""
+             )}
+            </div>
+        </article>
+      `;
+  }
+}
+
+customElements.define("skills-list", SkillsList);
