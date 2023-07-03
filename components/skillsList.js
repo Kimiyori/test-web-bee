e6d880797f7b71fe181718c82689055f58588919
Maxim <maksimkalin@mail.ru>
2023-07-03 21:37:48 +0300

    fix: удалил лишний код

--- components/skillsList.js
+++ components/skillsList.js
@@ -1,6 +1,3 @@
-function escapeSpecialChars(jsonString) {
-  return jsonString.replace(/\n/g, "\\n").replace(/\r/g, "\\r").replace(/\t/g, "\\t").replace(/\f/g, "\\f");
-}
 class SkillsList extends HTMLElement {
   connectedCallback() {
     this.render();
